29ab5968d1ffe20108ef71614d575231d0a0e305
农翔宇 <nongxiangyu@bytedance.com>
2022-08-22 10:52:32 +0800

    feat(function): 通用函数

--- function/function.go
+++ function/function.go
@@ -23,3 +23,29 @@ func DerefOrDef[T any, P *T](ptr P, d T) (v T) {
 	}
 	return *ptr
 }
+
+// Ternary 三元表达式
+func Ternary[T any](cond bool, v1, v2 T) T {
+	if cond {
+		return v1
+	}
+	return v2
+}
+
+// TernaryLazy 使用函数闭包提供值的三元表达式
+func TernaryLazy[T any](cond bool, supplier1, supplier2 Supplier[T]) T {
+	if cond {
+		return supplier1()
+	}
+	return supplier2()
+}
+
+// NonNil 判断是否是nil指针
+func NonNil[T any](t *T) bool {
+	return t != nil
+}
+
+// Ref 传入一个值，返回它的指针
+func Ref[T any](t T) *T {
+	return &t
+}
