7890c9604197b0dc10eadb8961fd949b3b464405
农翔宇 <nongxiangyu@bytedance.com>
2022-10-23 15:28:20 +0800

    feat(assert): 增加一个链式assert的接口

--- assert/tx.go
+++ assert/tx.go
@@ -8,3 +8,25 @@ func Should(conds ...func() error) error {
 	}
 	return nil
 }
+
+type Asserter struct {
+	conds []func() error
+}
+
+func NewAsserter() *Asserter {
+	return &Asserter{}
+}
+
+func (a *Asserter) With(f func() error) *Asserter {
+	a.conds = append(a.conds, f)
+	return a
+}
+
+func (a *Asserter) Check() error {
+	for _, c := range a.conds {
+		if err := c(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
